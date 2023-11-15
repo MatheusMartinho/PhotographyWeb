865554f504d08cc794c90b7f70a772025f362971
Matheus Moura Martinho <matheusmouramartinho@hotmail.com>
2023-11-15 00:08:58 -0300

    Databinging na pagina home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  title = 'Front-End Angular';
+  occupation = 'Developer & Photographer';
+  description = "Hi, I'm Matheus Martinho. A passionate Front-end Angular Developer & photographer. Based in Sao Paulo, Brazil";
+  linkedinUrl = 'https://br.linkedin.com/in/matheus-moura-martinho-8630091b3';
+  githubUrl = 'https://github.com/MatheusMartinho';
 }
