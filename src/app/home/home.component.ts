233044375a2239f9f73bb9bdd98a67e54f98752d
Matheus Moura Martinho <matheusmouramartinho@hotmail.com>
2023-11-16 16:24:52 -0300

    Remodelagem ta pagina inicial e do navbar para desktop

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,9 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  title = 'Front-End Angular';
-  occupation = 'Developer & Photographer';
-  description = "Hi, I'm Matheus Martinho. A passionate Front-end Angular Developer & photographer. Based in Sao Paulo, Brazil";
+  paragraph = 'Front-End Angular\n Developer &\n Photographer';
+  description = "Hi, I'm Matheus Martinho. A passionate Front-end Angular Developer &\n photographer. Based in Sao Paulo, Brazil";
   linkedinUrl = 'https://br.linkedin.com/in/matheus-moura-martinho-8630091b3';
   githubUrl = 'https://github.com/MatheusMartinho';
+
+  icons = [
+    {logo: 'fa-brands fa-js'},
+    {logo: "fa-brands fa-html5"},
+    {logo: "fa-brands fa-css3-alt"},
+    {logo: "fa-brands fa-angular"},
+    {logo: "fa-brands fa-sass"}
+  ]
 }
