4e0cfa43e1acd326bf959ddb5dec3e7064d1b848
Matheus Moura Martinho <matheusmouramartinho@hotmail.com>
2023-11-17 19:23:02 -0300

    scroll smooth added

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  scrollToAbout(): void {
+    const aboutElement = document.querySelector('#about');
+    if (aboutElement) {
+      aboutElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
